2f4be8c51992b0cbc5d075056ab500d674696966
Saiyyad Mohammad Adil <replyadil.96@gmail.com>
2023-03-28 12:34:55 +0530

    updated templates

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+habits = ["Coding","Programming"]
+
+@app.context_processor
+def create_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3,4)]
+        return dates
+    return {"date_range": date_range}
+
+@app.route('/')
+def index():
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+
+    return render_template('index.html', 
+                           habits=habits, 
+                           title = "Habit Tracker - Home", 
+                           selected_date=selected_date)
+
+@app.route('/add', methods=["GET", "POST"])
+def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get('habit'))
+    return render_template('add_habit.html', 
+                           title = 'Habit Tracker - Add Habit',
+                           selected_date = datetime.date.today())
